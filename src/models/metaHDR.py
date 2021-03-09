dd3c86fd1ca685e87b5393ad6cc98178bb34e39b
Edwin Pan <edwin.h.pan@gmail.com>
2021-03-08 22:46:58 -0800

    debug

--- src/models/metaHDR.py
+++ src/models/metaHDR.py
@@ -116,8 +116,10 @@ class MetaHDR(tf.keras.Model):
             # import pdb; pdb.set_trace()
 
             task_accuracy_tr_pre = 0
-            for i in range(label_tr.shape[0]):
+            N = label_tr.shape[0]
+            for i in range(N):
                 task_accuracy_tr_pre += self.ssim_score(label_tr[0].numpy(),task_output_tr_pre[i].numpy())
+            task_accuracy_tr_pre /= N
 
             for j in range(num_inner_updates):
                 task_accuracies_ts.append(self.ssim_score(label_ts.numpy().squeeze(),task_outputs_ts[j].numpy().squeeze()))
