71c2ff2519e693161164e5c5f100a241e70d4553
Anthony <anthony.vento@utexas.edu>
2021-03-14 06:10:02 -0700

    working l2l

--- src/models/torchmetaHDR.py
+++ src/models/torchmetaHDR.py
@@ -32,7 +32,7 @@ class UNet(nn.Module):
         
         self.expand2 = expand_block(4*num_filters, 2*num_filters, activation_function)
         
-        self.top = top_block(2*num_filters, num_filters, activation_function)
+        self.top = top_block(2*num_filters, 3, activation_function)
 
         # Define Pooling Operator
         self.pool = torch.nn.MaxPool2d((2,2), stride=2)
@@ -81,26 +81,26 @@ class UNet(nn.Module):
 
     
 
-def train_maml(cfg):
+def train_maml(cfg):    
     dg = DataGenerator(num_exposures=cfg.TRAIN.NUM_EXPOSURES)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     lr=0.005
     maml_lr=0.01
     
-    model = UNet(in_size=512, out_size=512, num_filters=8)
+    model = UNet(in_size=3, out_size=3, num_filters=8).double()
     model.to(device)
     meta_model = l2l.algorithms.MAML(model, lr=maml_lr)
     opt = optim.Adam(meta_model.parameters(), lr=lr)
     loss_func = nn.MSELoss()
     
-    tps = 32
-    fas= 5
+    fas=3
     iterations = 100
     
-    ssim = SSIM().cuda if device == 'cuda' else SSIM()
+    ssim = SSIM().double().cuda() if device == 'cuda' else SSIM().double()
     
     # Reference https://github.com/learnables/learn2learn/blob/master/examples/vision/meta_mnist.py
     for iteration in range(iterations):
+        print("ITERATION #", iteration)
         iteration_error = 0.0
         # iteration_acc = 0.0
         iteration_ssim = 0
@@ -110,13 +110,13 @@ def train_maml(cfg):
         test = torch.from_numpy(test).to(device)
         
         for batch_index in range(cfg.TRAIN.BATCH_SIZE):
+            print("Batch", batch_index)
             learner = meta_model.clone()
             
             # Separate data into adaptation/evalutation sets
-            adaptation_data, adaptation_labels = train[0, batch_index, ...], train[1, batch_index, ...]
-            evaluation_data, evaluation_labels = test[0, batch_index, ...], test[1, batch_index, ...]
-            # import pdb; pdb.set_trace()
-            
+            adaptation_data, adaptation_labels = train[0, batch_index, ...].permute(0, 3, 1, 2), train[1, batch_index, ...].permute(0, 3, 1, 2)
+            evaluation_data, evaluation_labels = test[0, batch_index, ...].permute(0, 3, 1, 2), test[1, batch_index, ...].permute(0, 3, 1, 2)
+
             # Fast Adaptation
             for step in range(fas):
                 train_error = loss_func(learner(adaptation_data), adaptation_labels)
@@ -126,17 +126,17 @@ def train_maml(cfg):
             predictions = learner(evaluation_data)
             valid_error = loss_func(predictions, evaluation_labels)
             valid_error /= len(evaluation_data)
-            valid_ssim = 0
-            for index in range(predictions.size(0)):
-                valid_ssim += ssim(predictions[index], evaluation_labels[index])
-            valid_ssim /= predictions.size(0)
+            
+            # Will return avg ssim 
+            valid_ssim = ssim(predictions, torch.clip(evaluation_labels, 0, 1)).item()
+            
             iteration_error += valid_error
             iteration_ssim += valid_ssim
     
-        iteration_error /= tps
+        iteration_error /= cfg.TRAIN.BATCH_SIZE
         # iteration_acc /= tps
-        iteration_ssim /= tps
-        print('Loss : {:.3f} SSIM : {:.3f}'.format(iteration_error.item(), valid_ssim))
+        iteration_ssim /= cfg.TRAIN.BATCH_SIZE
+        print('Train Loss : {:.3f} Train SSIM : {:.3f}'.format(iteration_error.item(), valid_ssim))
 
         # Take the meta-learning step
         opt.zero_grad()
