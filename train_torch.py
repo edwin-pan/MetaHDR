f7308998ca1d29c11712e7a6da200dff7d617dc3
Edwin Pan <edwin.h.pan@gmail.com>
2021-03-14 16:46:01 -0700

    new entry for torch only

---
+++ train_torch.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import GPUtil
+import numpy as np
+import time
+import os
+
+from src.dataset.dataloader import DataGenerator
+from src.core.config import parse_args
+from src.core.utils import prepare_output_dir
+
+from src.models.torchmetaHDR import train_maml
+
+def main(cfg):
+    train_maml(cfg)
+
+
+if __name__ == '__main__':
+    cfg, cfg_file = parse_args()
+    cfg = prepare_output_dir(cfg, cfg_file)
+
+    main(cfg)
