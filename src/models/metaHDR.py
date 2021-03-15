2d2b741cf5f20c92e23292684c1885fd7c908b5f
Edwin Pan <edwin.h.pan@gmail.com>
2021-03-15 12:27:14 -0700

    debug

--- src/models/metaHDR.py
+++ src/models/metaHDR.py
@@ -56,121 +56,121 @@ def outer_eval_step(inp, model, meta_batch_size=25, num_inner_updates=1):
     # tf.keras.backend.clear_session()
     return outputs_tr, outputs_ts, total_loss_tr_pre, total_losses_ts, total_accuracy_tr_pre, total_accuracies_ts
 
-class MetaHDR(tf.keras.Model):
-    def __init__(self,
-                 loss_func,
-                 img_width=1,
-                 img_height=1,
-                 num_inner_updates=1,
-                 inner_update_lr=0.4,
-                 pretrain_flag=False,
-                 model_weights=None):
-        """
-        Main definition of MetaHDR model. MetaHDR consists of a core UNet architecture wrapped with the MAML framework. The inner loop is
-        defined within the MetaHDR class. The outerloop meta-parameter tuning is handled outside (see train.py)
-        """
-        super(MetaHDR, self).__init__()
-        self.width = img_width
-        self.height = img_height
-        self.inner_update_lr = inner_update_lr
-        self.ssim_score = tf.image.ssim
-        self.k_shot=1 # This won't change, because we only have 1 example per task
-        self.pretrain_flag = pretrain_flag
-        self.loss_func = loss_func
-        self.non_trainable_layers =  [0,3,6,9,12,16,20,24,28]
-        self.up_conv_layers =  [15,19,23,27]
-        if self.pretrain_flag:
-            print(self.width,self.height)
-            self.m = get_unet(self.height, self.width)
-            self.m.load_weights(model_weights)
-            self.m.build((self.height, self.width, 3))
-            print("Loaded weights from: {}".format(model_weights))
-        else:
-            self.m = get_unet(self.height, self.width)
-            self.m.build((self.height, self.width, 3))
+# class MetaHDR(tf.keras.Model):
+#     def __init__(self,
+#                  loss_func,
+#                  img_width=1,
+#                  img_height=1,
+#                  num_inner_updates=1,
+#                  inner_update_lr=0.4,
+#                  pretrain_flag=False,
+#                  model_weights=None):
+#         """
+#         Main definition of MetaHDR model. MetaHDR consists of a core UNet architecture wrapped with the MAML framework. The inner loop is
+#         defined within the MetaHDR class. The outerloop meta-parameter tuning is handled outside (see train.py)
+#         """
+#         super(MetaHDR, self).__init__()
+#         self.width = img_width
+#         self.height = img_height
+#         self.inner_update_lr = inner_update_lr
+#         self.ssim_score = tf.image.ssim
+#         self.k_shot=1 # This won't change, because we only have 1 example per task
+#         self.pretrain_flag = pretrain_flag
+#         self.loss_func = loss_func
+#         self.non_trainable_layers =  [0,3,6,9,12,16,20,24,28]
+#         self.up_conv_layers =  [15,19,23,27]
+#         if self.pretrain_flag:
+#             print(self.width,self.height)
+#             self.m = get_unet(self.height, self.width)
+#             self.m.load_weights(model_weights)
+#             self.m.build((self.height, self.width, 3))
+#             print("Loaded weights from: {}".format(model_weights))
+#         else:
+#             self.m = get_unet(self.height, self.width)
+#             self.m.build((self.height, self.width, 3))
 
-    # @tf.function
-    def task_inner_loop(self, inp,reuse=True,meta_batch_size=25,num_inner_updates=1):
-        # the inner and outer loop data, query set: (input_tr,label_tr)
-        input_tr, input_ts, label_tr, label_ts = inp[0], inp[1], inp[2], inp[3]
-        task_output_tr_pre, task_loss_tr_pre, task_accuracy_tr_pre = None, None, None
-        task_outputs_ts, task_losses_ts, task_accuracies_ts = [], [], []
+#     # @tf.function
+#     def task_inner_loop(self, inp,reuse=True,meta_batch_size=25,num_inner_updates=1):
+#         # the inner and outer loop data, query set: (input_tr,label_tr)
+#         input_tr, input_ts, label_tr, label_ts = inp[0], inp[1], inp[2], inp[3]
+#         task_output_tr_pre, task_loss_tr_pre, task_accuracy_tr_pre = None, None, None
+#         task_outputs_ts, task_losses_ts, task_accuracies_ts = [], [], []
 
-        with tf.GradientTape(persistent=True) as tape:
+#         with tf.GradientTape(persistent=True) as tape:
 
-            get_GPU_usage("inner pre")
-            # tf.keras.backend.clear_session()
-            # gc.collect()
-            # get_GPU_usage("intermediate post")
-            task_output_tr_pre = unet_forward(self.m, input_tr)
-            get_GPU_usage("inner post")
+#             get_GPU_usage("inner pre")
+#             # tf.keras.backend.clear_session()
+#             # gc.collect()
+#             # get_GPU_usage("intermediate post")
+#             task_output_tr_pre = unet_forward(self.m, input_tr)
+#             get_GPU_usage("inner post")
 
-            inner_task_weights = [item for item in self.m.trainable_weights]
+#             inner_task_weights = [item for item in self.m.trainable_weights]
 
-            task_loss_tr_pre = self.loss_func(label_tr,task_output_tr_pre)
-            grads = tape.gradient(task_loss_tr_pre,inner_task_weights)
+#             task_loss_tr_pre = self.loss_func(label_tr,task_output_tr_pre)
+#             grads = tape.gradient(task_loss_tr_pre,inner_task_weights)
 
-            # Apply the gradients
-            k=0
-            for j in range(len(self.m.layers)):
-                # print(j,self.m.layers[j].name)
-                if j not in self.non_trainable_layers: # Layers w/ no trainable parameters
-                    if j in self.up_conv_layers: # Up-conv layers
-                        self.m.layers[j].kernel=self.m.layers[j].kernel - self.inner_update_lr*grads[k]
-                        self.m.layers[j].bias=self.m.layers[j].bias - self.inner_update_lr*grads[k+1]
-                        k+=2
-                        self.m.layers[j].trainable=True
-                    else: # Regular conv layers
-                        self.m.layers[j].depthwise_kernel=self.m.layers[j].depthwise_kernel - self.inner_update_lr*grads[k]
-                        self.m.layers[j].pointwise_kernel=self.m.layers[j].pointwise_kernel - self.inner_update_lr*grads[k+1]
-                        self.m.layers[j].bias=self.m.layers[j].bias - self.inner_update_lr*grads[k+2]
-                        k+=3
-                        self.m.layers[j].trainable=True
+#             # Apply the gradients
+#             k=0
+#             for j in range(len(self.m.layers)):
+#                 # print(j,self.m.layers[j].name)
+#                 if j not in self.non_trainable_layers: # Layers w/ no trainable parameters
+#                     if j in self.up_conv_layers: # Up-conv layers
+#                         self.m.layers[j].kernel=self.m.layers[j].kernel - self.inner_update_lr*grads[k]
+#                         self.m.layers[j].bias=self.m.layers[j].bias - self.inner_update_lr*grads[k+1]
+#                         k+=2
+#                         self.m.layers[j].trainable=True
+#                     else: # Regular conv layers
+#                         self.m.layers[j].depthwise_kernel=self.m.layers[j].depthwise_kernel - self.inner_update_lr*grads[k]
+#                         self.m.layers[j].pointwise_kernel=self.m.layers[j].pointwise_kernel - self.inner_update_lr*grads[k+1]
+#                         self.m.layers[j].bias=self.m.layers[j].bias - self.inner_update_lr*grads[k+2]
+#                         k+=3
+#                         self.m.layers[j].trainable=True
 
-            output_ts = unet_forward(self.m, input_ts)
-            # output_ts = self.m.predict_on_batch(input_ts)
+#             output_ts = unet_forward(self.m, input_ts)
+#             # output_ts = self.m.predict_on_batch(input_ts)
 
-            loss_ts = self.loss_func(label_ts,output_ts)
-            task_outputs_ts.append(output_ts)
-            task_losses_ts.append(loss_ts)
+#             loss_ts = self.loss_func(label_ts,output_ts)
+#             task_outputs_ts.append(output_ts)
+#             task_losses_ts.append(loss_ts)
             
-            # Now revert the gradients
-            k=0
-            for j in range(len(self.m.layers)):
-                # print(j,self.m.layers[j].name)
-                if j not in self.non_trainable_layers: # Layers w/ no trainable parameters
-                    if j in self.up_conv_layers: # Up-conv layers
-                        self.m.layers[j].kernel=inner_task_weights[k]
-                        self.m.layers[j].bias=inner_task_weights[k+1]
-                        k+=2
-                        self.m.layers[j].trainable=True
-                    else: # Regular conv layers
-                        self.m.layers[j].depthwise_kernel=inner_task_weights[k]
-                        self.m.layers[j].pointwise_kernel=inner_task_weights[k+1]
-                        self.m.layers[j].bias=inner_task_weights[k+2]
-                        k+=3
-                        self.m.layers[j].trainable=True
+#             # Now revert the gradients
+#             k=0
+#             for j in range(len(self.m.layers)):
+#                 # print(j,self.m.layers[j].name)
+#                 if j not in self.non_trainable_layers: # Layers w/ no trainable parameters
+#                     if j in self.up_conv_layers: # Up-conv layers
+#                         self.m.layers[j].kernel=inner_task_weights[k]
+#                         self.m.layers[j].bias=inner_task_weights[k+1]
+#                         k+=2
+#                         self.m.layers[j].trainable=True
+#                     else: # Regular conv layers
+#                         self.m.layers[j].depthwise_kernel=inner_task_weights[k]
+#                         self.m.layers[j].pointwise_kernel=inner_task_weights[k+1]
+#                         self.m.layers[j].bias=inner_task_weights[k+2]
+#                         k+=3
+#                         self.m.layers[j].trainable=True
             
-        # Compute accuracies from output predictions
-        task_accuracy_tr_pre = tf.reduce_mean(self.ssim_score(label_tr,task_output_tr_pre, 1.0))
+#         # Compute accuracies from output predictions
+#         task_accuracy_tr_pre = tf.reduce_mean(self.ssim_score(label_tr,task_output_tr_pre, 1.0))
         
-        for j in range(num_inner_updates):
-            task_accuracies_ts.append(self.ssim_score(label_ts,task_outputs_ts[j], 1.0))
+#         for j in range(num_inner_updates):
+#             task_accuracies_ts.append(self.ssim_score(label_ts,task_outputs_ts[j], 1.0))
 
-        task_output = [task_output_tr_pre, task_outputs_ts, task_loss_tr_pre, task_losses_ts, task_accuracy_tr_pre, task_accuracies_ts]
+#         task_output = [task_output_tr_pre, task_outputs_ts, task_loss_tr_pre, task_losses_ts, task_accuracy_tr_pre, task_accuracies_ts]
 
-        return task_output
+#         return task_output
 
-    # @tf.function
-    def call(self,
-             inp,
-             meta_batch_size=25,
-             num_inner_updates=1):
-        input_tr, input_ts, label_tr, label_ts = inp
-        out_dtype = [tf.float32, [tf.float32]*num_inner_updates, tf.float32, [tf.float32]*num_inner_updates]
-        out_dtype.extend([tf.float32, [tf.float32]*num_inner_updates])
-        task_inner_loop_partial = partial(self.task_inner_loop, meta_batch_size=meta_batch_size, num_inner_updates=num_inner_updates)
-        result = tf.map_fn(task_inner_loop_partial, elems=(input_tr, input_ts, label_tr, label_ts),
-                        dtype=out_dtype,
-                        parallel_iterations=meta_batch_size)
-        return result
+#     # @tf.function
+#     def call(self,
+#              inp,
+#              meta_batch_size=25,
+#              num_inner_updates=1):
+#         input_tr, input_ts, label_tr, label_ts = inp
+#         out_dtype = [tf.float32, [tf.float32]*num_inner_updates, tf.float32, [tf.float32]*num_inner_updates]
+#         out_dtype.extend([tf.float32, [tf.float32]*num_inner_updates])
+#         task_inner_loop_partial = partial(self.task_inner_loop, meta_batch_size=meta_batch_size, num_inner_updates=num_inner_updates)
+#         result = tf.map_fn(task_inner_loop_partial, elems=(input_tr, input_ts, label_tr, label_ts),
+#                         dtype=out_dtype,
+#                         parallel_iterations=meta_batch_size)
+#         return result
