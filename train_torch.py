aebb5e9ed180db9478f703f297004b132e57143d
Edwin Pan <edwin.h.pan@gmail.com>
2021-03-15 15:52:26 -0700

    debug

--- train_torch.py
+++ train_torch.py
@@ -7,7 +7,7 @@ from src.models.torchmetaHDR import train_maml
 from src.core.utils import create_logger
 
 def main(cfg, log_dir):
-    logger = create_logger(cfg.LOGDIR, phase='train')
+    logger = create_logger(log_dir, phase='train')
 
     logger.info(f'GPU name -> {torch.cuda.get_device_name()}')
     logger.info(f'GPU feat -> {torch.cuda.get_device_properties("cuda")}')
