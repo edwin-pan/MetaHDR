fa82dfcff250e5625f1a8059f2d4adee225be39d
Edwin Pan <edwin.h.pan@gmail.com>
2021-03-17 00:43:32 -0700

    debug

--- eval.py
+++ eval.py
@@ -68,72 +68,72 @@ def main(args):
 
     all_test_data = dg.meta_test_data
 
-    # # Perform single-shot evaluation
-    # print("Performing Single-Shot Evaluation")
-    # eval_single_ssim = 0.0
-    # eval_single_psnr = 0.0
-    # idx = 0
-    # for i in tqdm(range(all_test_data.shape[0])):
-    #     for j in range(1, all_test_data.shape[1]):
-    #         input_test_image = all_test_data[np.newaxis, i, j]
-    #         input_test_label = all_test_data[np.newaxis, i, 0]
-    #         _, test_ssim, test_psnr = evaluate_single_maml(meta_model, loss_func, input_test_image, input_test_label, idx, device=device, visualize_flag=True, visualize_dir=single_dir)
+    # Perform single-shot evaluation
+    print("Performing Single-Shot Evaluation")
+    eval_single_ssim = 0.0
+    eval_single_psnr = 0.0
+    idx = 0
+    for i in tqdm(range(all_test_data.shape[0])):
+        for j in range(1, all_test_data.shape[1]):
+            input_test_image = all_test_data[np.newaxis, i, j]
+            input_test_label = all_test_data[np.newaxis, i, 0]
+            _, test_ssim, test_psnr = evaluate_single_maml(meta_model, loss_func, input_test_image, input_test_label, idx, device=device, visualize_flag=True, visualize_dir=single_dir)
 
-    #         eval_single_ssim+=test_ssim
-    #         eval_single_psnr+=test_psnr
-    #         idx += 1
-    # eval_single_ssim /= (all_test_data.shape[0]*(all_test_data.shape[1]-1))
-    # eval_single_psnr /= (all_test_data.shape[0]*(all_test_data.shape[1]-1))
-    # print("[Evaluation Results] Average Single-Shot Evaluation SSIM : {:.3f}".format(eval_single_ssim))
-    # print("[Evaluation Results] Average Single-Shot Evaluation PSNR : {:.3f}".format(eval_single_psnr))
+            eval_single_ssim+=test_ssim
+            eval_single_psnr+=test_psnr
+            idx += 1
+    eval_single_ssim /= (all_test_data.shape[0]*(all_test_data.shape[1]-1))
+    eval_single_psnr /= (all_test_data.shape[0]*(all_test_data.shape[1]-1))
+    print("[Evaluation Results] Average Single-Shot Evaluation SSIM : {:.3f}".format(eval_single_ssim))
+    print("[Evaluation Results] Average Single-Shot Evaluation PSNR : {:.3f}".format(eval_single_psnr))
 
-    # # Perform adaptive evaluation
-    # print("Performing Adaptive Evaluation using Debevec labels")
-    # eval_adaptive_ssim = 0.0
-    # eval_adaptive_psnr = 0.0
-    # idx = 0
-    # for curr_idx in tqdm(range(all_test_data.shape[0])):
-    #     cur_batch = all_test_data[np.newaxis, curr_idx]
-    #     tr_images, ts_images = [], []
-    #     tr_labels, ts_labels = [], []
-    #     for image_set in cur_batch:
-    #         # Train and Test for each set of exposures
-    #         tr, ts = train_test_split(np.arange(1, cfg.EVAL.NUM_EXPOSURES+1), test_size=1)
+    # Perform adaptive evaluation
+    print("Performing Adaptive Evaluation using Debevec labels")
+    eval_adaptive_ssim = 0.0
+    eval_adaptive_psnr = 0.0
+    idx = 0
+    for curr_idx in tqdm(range(all_test_data.shape[0])):
+        cur_batch = all_test_data[np.newaxis, curr_idx]
+        tr_images, ts_images = [], []
+        tr_labels, ts_labels = [], []
+        for image_set in cur_batch:
+            # Train and Test for each set of exposures
+            tr, ts = train_test_split(np.arange(1, cfg.EVAL.NUM_EXPOSURES+1), test_size=1)
             
-    #         cur_tr_images, cur_tr_labels = [], []
-    #         for i in tr:
-    #             cur_tr_images.append(image_set[i, ...])
-    #             cur_tr_labels.append(image_set[0, ...])
-    #         tr_images.append(np.stack(cur_tr_images))
-    #         tr_labels.append(np.stack(cur_tr_labels))
+            cur_tr_images, cur_tr_labels = [], []
+            for i in tr:
+                cur_tr_images.append(image_set[i, ...])
+                cur_tr_labels.append(image_set[0, ...])
+            tr_images.append(np.stack(cur_tr_images))
+            tr_labels.append(np.stack(cur_tr_labels))
             
-    #         cur_ts_images, cur_ts_labels = [], []
-    #         for i in ts:
-    #             cur_ts_images.append(image_set[i, ...])
-    #             cur_ts_labels.append(image_set[0, ...])
-    #         ts_images.append(np.stack(cur_ts_images))
-    #         ts_labels.append(np.stack(cur_ts_labels))
+            cur_ts_images, cur_ts_labels = [], []
+            for i in ts:
+                cur_ts_images.append(image_set[i, ...])
+                cur_ts_labels.append(image_set[0, ...])
+            ts_images.append(np.stack(cur_ts_images))
+            ts_labels.append(np.stack(cur_ts_labels))
         
-    #     tr_images = np.stack(tr_images)
-    #     tr_labels = np.stack(tr_labels)
-    #     ts_images = np.stack(ts_images)
-    #     ts_labels = np.stack(ts_labels)
+        tr_images = np.stack(tr_images)
+        tr_labels = np.stack(tr_labels)
+        ts_images = np.stack(ts_images)
+        ts_labels = np.stack(ts_labels)
         
-    #     eval_train = np.stack([tr_images, tr_labels])
-    #     eval_test = np.stack([ts_images, ts_labels])
+        eval_train = np.stack([tr_images, tr_labels])
+        eval_test = np.stack([ts_images, ts_labels])
 
-    #     # import pdb; pdb.set_trace()
-    #     _, test_ssim, test_psnr = evaluate_maml(meta_model, loss_func, eval_train, eval_test, idx, cfg.EVAL.NUM_TASK_TR_ITER, device=device, visualize_flag=True, visualize_dir=adapt_debevec_dir)
-    #     idx += 1
+        # import pdb; pdb.set_trace()
+        _, test_ssim, test_psnr = evaluate_maml(meta_model, loss_func, eval_train, eval_test, idx, cfg.EVAL.NUM_TASK_TR_ITER, device=device, visualize_flag=True, visualize_dir=adapt_debevec_dir)
+        idx += 1
         
-    #     eval_adaptive_ssim += test_ssim
-    #     eval_adaptive_psnr += test_psnr
+        eval_adaptive_ssim += test_ssim
+        eval_adaptive_psnr += test_psnr
 
-    # eval_adaptive_ssim /= all_test_data.shape[0]
-    # eval_adaptive_psnr /= all_test_data.shape[0]
+    eval_adaptive_ssim /= all_test_data.shape[0]
+    eval_adaptive_psnr /= all_test_data.shape[0]
 
-    # print("[Evaluation Results] Average Debevec Adapted Evaluation SSIM : {:.3f}".format(eval_adaptive_ssim))
-    # print("[Evaluation Results] Average Debevec Adapted Evaluation PSNR : {:.3f}".format(eval_adaptive_psnr))
+    print("[Evaluation Results] Average Debevec Adapted Evaluation SSIM : {:.3f}".format(eval_adaptive_ssim))
+    print("[Evaluation Results] Average Debevec Adapted Evaluation PSNR : {:.3f}".format(eval_adaptive_psnr))
 
     # Perform adaptive evaluation
     print("Performing Adaptive Evaluation using HDRCNN labels")
