ae80c91c621eb8fbb18fe3fdd7653920764fcb43
Anthony <anthony.vento@utexas.edu>
2021-03-19 01:43:54 -0700

    modify lpips loss

--- src/core/loss.py
+++ src/core/loss.py
@@ -2,6 +2,7 @@ import numpy as np
 from piqa import SSIM, LPIPS, HaarPSI
 from torch import nn
 import torch
+import lpips
 
 ['ExpandNetLoss', 'HaarLoss', 'LPIPSLoss', 'LPIPSLoss_L2', 'SSIMLoss']
 
@@ -54,14 +55,16 @@ class HaarLoss(nn.Module):
         return (1 - self.loss_lambda) * self.l2_loss(x, y) + self.loss_lambda * (1 - sim)
 
 class LPIPSLoss(nn.Module):
-    def __init__(self, loss_lambda=0.5):
+    def __init__(self):
         super(LPIPSLoss, self).__init__()
-        self.similarity = LPIPS(network='vgg').cuda()
-        self.l2_loss = nn.MSELoss()
-        self.loss_lambda = loss_lambda
+        self.similarity = lpips.LPIPS(net='vgg')
+        # self.similarity = LPIPS(network='vgg').cuda()
+        # self.l2_loss = nn.MSELoss()
+        # self.loss_lambda = loss_lambda
 
     def forward(self, x, y):
-        sim = self.similarity(x.float(), y.float())
+        # norm to -1, 1
+        sim = self.similarity((x / 0.5 - 1).float(), (y / 0.5 - 1).float())
         return sim
 
 class LPIPSLoss_L2(nn.Module):
